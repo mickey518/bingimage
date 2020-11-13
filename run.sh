3f12602c5a89cf85728d347d132b9004aa7b7bec
wangmeng <wangmeng@zhuojianchina.com>
2020-11-13 22:53:48 +0800

    增加 log 日志配置

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --restart=always -d -p 1000:1000 -v /Users/wangmeng/OneDrive/pictures/BingImages/images:/usr/local/bing/images -m 200M --name bingimage bingimage:1.0
+docker run --restart=always -d -p 1000:1000 -v /Users/wangmeng/OneDrive/pictures/BingImages/images:/usr/local/bing/images -v /Users/wangmeng/OneDrive/pictures/BingImages/logs/:/usr/local/bing/logs/ -m 200M --name bingimage bingimage:1.0
